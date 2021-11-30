25ef5e0ea054f397d810927f92fa598ece3014b3
Teodor Jenkler <32197435+TeodorJenkler@users.noreply.github.com>
2021-11-30 14:35:30 +0100

    update.coffeemachine.php
    milk and sugar to caffee

--- coffeemachine.php
+++ coffeemachine.php
@@ -2,7 +2,9 @@
 <html lang="sv">
 <head>
 <title>Kaffemaskin</title>
-<?php
+  </head>
+  <body>
+  <?php
 //Check if sent
 if(isset($_POST['issubmit']) && $_POST['issubmit'] == 1){
 $choice = $_POST['choice'];
@@ -11,23 +13,47 @@ $issubmit=1;
 else{
 $choice="";
 $issubmit="0";
+?>
+<form action="coffeemachine.php" method="post">
+<input type="hidden" name="issubmit" value="1">
+<input type="text" name="choice" placeholder="K/T">
+<input type="submit" value="brew">
+</form>  
 }
+<?php    
 //RESULT
+if ($issubmit == 1){
 if ($choice == "K" || $choice == "k"){
 echo "Här är kaffe";
+if ($extrachoice == "milk"){
+echo "Kaffe med mjölk";
 }
+elseif ($extrachoice =="sugar") {
+echo "Kaffe med socker";
+}
+else 
+?>
+<form action="coffeemachine.php" method="post">
+  <input type="hidden" name="issubmit" value="1">
+  <input type="hidden" name="issubmitextra" value="1">
+  <input type="hidden" name"choice" value= <?php echo $choice; ?>>
+  välj mjölk
+  <input type="radio" id="milk" name="extra" value="milk">
+  <label for="milk">Mjölk</label><br>
+  Eller socker
+  <input type="radio" id="sugar" name="extra" value="sugar">
+  <label for="sugar">Socker</label><br>
+  <input type="submit" name="send">
+ </form>
+}
+<?php    
 else if ($choice =="T" || $choice == "t"){
 echo "Här är Te";
 }
 else{
 echo "Välj Kaffe eller TE!";
 }
-</head>
-<body>
-<form action="coffeemachine.php" method="post">
-<input type="hidden" name="issubmit" value="1">
-<p><input type="text" name="choice" placeholder="K/T"></p>
-<p><input type="submit" value="brew"></p>
-</form>
+}
+?>
 </body>
 </html>
